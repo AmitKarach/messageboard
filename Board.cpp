4713d1919fd16ced36937d639d3f80bff3a337e1
AmitKarach <karach.amit30@gmail.com>
2021-03-22 21:43:44 +0200

    done with task a

---
+++ Board.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Board.hpp"
+using namespace std;
+
+namespace ariel
+{
+    void Board::post(unsigned int row, unsigned int column, Direction dierction, string info )
+    {
+        
+    }
+
+    string Board::read(unsigned int row, unsigned int column, Direction dierction, unsigned int howMuch )
+    {
+        return " ";
+    }
+
+    void Board::show()
+    {
+        
+    }
+} 
+
