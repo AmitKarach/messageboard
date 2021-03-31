c74c81f2da625e162a84c336469efa944a259501
AmitKarach <karach.amit30@gmail.com>
2021-03-31 13:12:06 +0300

    done

--- Board.cpp
+++ Board.cpp
@@ -4,18 +4,68 @@ using namespace std;
 
 namespace ariel
 {
-    void Board::post(unsigned int row, unsigned int column, Direction dierction, string info )
+    void Board::post(unsigned int row, unsigned int column, Direction dierction, string message )
     {
+        if (dierction == Direction::Horizontal)
+        {
+            maxcols= max(maxcols,column+ unsigned(message.length()));
+        }
+        else
+        {
+            maxrow = max(maxrow,row+ unsigned(message.length()));
+        }
+        minrow= min(minrow,row);
+        mincols=min(mincols,column);
         
+        for (unsigned int i = 0; i < message.length(); i++)
+        {
+            board[row][column] = message.at(i);
+            if (dierction == Direction::Horizontal)
+            {
+               column++;
+            }
+            else
+            {
+                row++;
+            }         
+        }       
     }
 
     string Board::read(unsigned int row, unsigned int column, Direction dierction, unsigned int howMuch )
     {
-        return " ";
+        string ans;
+        for (int i = 0; i < howMuch; i++)
+        {
+            if(board.find(row) == board.end() || board[row].find(column)== board[row].end())
+            {
+                ans.append("_");
+            }
+            else{
+                ans.append(board[row][column]);
+            }
+            if (dierction == Direction::Horizontal)
+            {
+               column++;
+            }
+            else
+            {
+                row++;
+            } 
+        }
+        
+        return ans;
     }
 
     void Board::show()
     {
+        for (unsigned int i = minrow; i < maxrow; i++)
+        {
+            for (unsigned int j = mincols; j < maxcols; j++)
+            {
+                cout << board[i][j] <<" ";    
+            }
+            cout<<endl;
+        }
         
     }
 } 
--- Board.hpp
+++ Board.hpp
@@ -1,15 +1,20 @@
 #include <string>
 #include "Direction.hpp"
+#include <map>
 
 using namespace std;
 
 namespace ariel{
     class Board{
-        unsigned int rows;
-        unsigned int cols;
+    private:
+        unsigned int maxrow=0;
+        unsigned int minrow=INT8_MAX;
+        unsigned int maxcols=0;
+        unsigned int mincols=INT8_MAX;
+        std::map<unsigned int,std::map<unsigned int,string>> board;
     public:
-        static void post(unsigned int row, unsigned int column, Direction dierction, string info);
-        static string read(unsigned int row, unsigned int column, Direction dierction, unsigned int howMuch);
+        void post(unsigned int row, unsigned int column, Direction dierction, string message);
+        string read(unsigned int row, unsigned int column, Direction dierction, unsigned int howMuch);
         void show();
     };
 }
