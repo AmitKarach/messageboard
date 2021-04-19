a6afe2e2ff92a96f98a8d9fb865d9fd78c22cdc5
AmitKarach <karach.amit30@gmail.com>
2021-04-19 12:53:41 +0300

    add notes and change max and min

--- Board.cpp
+++ Board.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 namespace ariel
 {
+    //here we are posting a note on the board
     void Board::post(unsigned int row, unsigned int column, Direction dierction, string message )
     {
+        //we are cheaching if this post is farther then the max note that there is on the board
+        //we will use that in Show()
         if (dierction == Direction::Horizontal)
         {
             maxcols= max(maxcols,column+ unsigned(message.length()-1));
@@ -19,6 +22,7 @@ namespace ariel
         minrow= min(minrow,row);
         mincols=min(mincols,column);
         
+        //we are going through the message and puting it on the board 
         for (unsigned int i = 0; i < message.length(); i++)
         {
             board[row][column] = message.at(i);
@@ -34,8 +38,10 @@ namespace ariel
             
     }
 
+    //here we are reading a post from the board
     string Board::read(unsigned int row, unsigned int column, Direction dierction, unsigned int howMuch )
     {
+        //we will add to this ans string every note we will find and if we get to a place there is no letter we put "_" insted
         string ans;
         for (int i = 0; i < howMuch; i++)
         {
@@ -58,7 +64,9 @@ namespace ariel
         
         return ans;
     }
-
+    //this will print the board we have 
+    //we are starting at the min messege and from there we will go up
+    //thats why if there is only a messege in line 1000 we wont start from line 0 and post "_" until we get to line 1000
     void Board::show()
     {
         for (unsigned int i = minrow; i <= maxrow; i++)
--- Board.hpp
+++ Board.hpp
@@ -1,5 +1,6 @@
 #include <string>
 #include "Direction.hpp"
+#include <climits>
 #include <map>
 
 using namespace std;
@@ -8,9 +9,9 @@ namespace ariel{
     class Board{
     private:
         unsigned int maxrow=0;
-        unsigned int minrow=INT8_MAX;
+        unsigned int minrow=UINT_MAX;
         unsigned int maxcols=0;
-        unsigned int mincols=INT8_MAX;
+        unsigned int mincols=UINT_MAX;
         std::map<unsigned int,std::map<unsigned int,string>> board;
     public:
         void post(unsigned int row, unsigned int column, Direction dierction, string message);
